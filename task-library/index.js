e0f900a1d5be52498351716cf9e2bba9237bb58a
Jamie Hutber <jamie@hutber.com>
2018-08-15 18:33:09 +0100

    test: add searchArray tests

--- task-library/index.js
+++ task-library/index.js
@@ -1,7 +1,12 @@
-const sortTransations = function(arrayToBeSorted, sortType = "amount"){
+const sortArray = function(arrayToBeSorted, sortType = "amount"){
   return Array.isArray(arrayToBeSorted) && [...arrayToBeSorted].sort((a,b) =>  a[sortType]-b[sortType]);
 };
 
+const searchArray = function(array, searchTerm, field= "name"){
+  return Array.isArray(array) && array.filter(val => val[field].includes(searchTerm));
+};
+
 module.exports = {
-  sortTransations
+  sortArray,
+  searchArray
 };
--- task-library/index.test.js
+++ task-library/index.test.js
@@ -1,13 +1,37 @@
-const { sortTransations } = require('./index');
+const { sortArray, searchArray } = require('./index');
 const { transactions } = require('../db.json');
 
-describe('sortTransations', () => {
-  const expectedArray = sortTransations(transactions);
+describe('searchArray', () => {
+  const searchTerm = '3223';
+  const expectedSearchResults =  {
+    "name": "Checking Account 3223",
+  };
+  const expectedArray = searchArray(transactions, searchTerm);
 
   test('The original data is an array', () => {
     expect(Array.isArray(expectedArray)).toBe(true);
   });
-  test('We have sorted the array', () => {
+  test('The array has Changed', () => {
     expect(JSON.stringify(transactions)).not.toBe(JSON.stringify(expectedArray));
+  });
+  test('We have the expected result', () => {
+    expect(expectedArray).toContainEqual(expect.objectContaining(expectedSearchResults));
+  });
+});
+
+
+describe('sortTransations', () => {
+  const expectedArray = sortArray(transactions);
+
+  test('The original data is an array', () => {
+    expect(Array.isArray(expectedArray)).toBe(true);
+  });
+  test('The array has Changed', () => {
+    expect(JSON.stringify(transactions)).not.toBe(JSON.stringify(expectedArray));
+  });
+  test('The array is sorted by amount', () => {
+    const firstResult = expectedArray[0].amount;
+    const secondResult = expectedArray[1].amount;
+    expect(secondResult).toBeGreaterThan(firstResult);
   });
 });
